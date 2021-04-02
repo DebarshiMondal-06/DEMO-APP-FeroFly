b5afe46eac0abdbf5e046cec36cbb057f98105f9
Debarshi <debopiku1122@gmail.com>
2021-04-02 09:51:54 +0530

    file

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  const itemsFun = (item) => {
+    return <li><Link className='nav-items'>{item}</Link></li>
+  }
+  return <section className='nav-section'>
+    <nav className='nav-container'>
+      <ul className='nav-list'>
+        {itemsFun('Home')}
+        {itemsFun('About')}
+        {itemsFun('Rooms')}
+        {itemsFun('Service')}
+        {itemsFun('Gallery')}
+        {itemsFun('Contact')}
+        <li><button className='btn'>Book a Room</button></li>
+      </ul>
+    </nav>
+  </section>
+}
+
+export default Navbar
