98fcd2d87923aa32a51943801c4a7c832fa6cadf
DEBARSHI MONDAL <52108126+LENO-DEV@users.noreply.github.com>
2021-11-13 23:23:05 +0530

    Update HeroSec.js

--- src/AfterNavSec1/HeroSec.js
+++ src/AfterNavSec1/HeroSec.js
@@ -17,7 +17,7 @@ const HeroSec = () => {
     <article className='hero-sec'>
       <div className='text-section'>
         <h1>{value}</h1>
-        <p>&nbsp;Build from Heart&nbsp;</p>
+        <p>&nbsp;Build from Heart. Made with Love&nbsp;</p>
       </div>
       <div className='img-section'>
         <div class="parallax-window" data-parallax="scroll" data-image-src="https://static.wixstatic.com/media/84770f_2d6f401da0284208a7ad5d274041dffd~mv2_d_2500_1658_s_2.jpg/v1/fill/w_980,h_731,al_c,q_85,usm_0.66_1.00_0.01/84770f_2d6f401da0284208a7ad5d274041dffd~mv2_d_2500_1658_s_2.webp"></div>
